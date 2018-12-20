6ecc2759d43674bb5ec3c7807b6c4d60f7a44ec6
Haku-Dell <g@kohaku.cc>
2018-12-20 17:32:38 +0800

    call api

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,23 +1,52 @@
 $(document).ready(function(){
-    $("#grid").kendoGrid({
-        sortable:true,
-        columns: [{
-            field: "FirstName",
-            title: "First Name"
-        },
-            {
-                field: "LastName",
-                title: "Last Name"
-            }],
-        dataSource: {
-            data: [{
-                FirstName: "Joe",
-                LastName: "Smith"
-            },
-                {
-                    FirstName: "Jane",
-                    LastName: "Smith"
-                }]
-        }
-    });
+    // $("#grid").kendoGrid({
+    //     sortable:true,
+    //     columns: [{
+    //         field: "FirstName",
+    //         title: "First Name"
+    //     },
+    //         {
+    //             field: "LastName",
+    //             title: "Last Name"
+    //         }],
+    //     dataSource: {
+    //         data: [{
+    //             FirstName: "Joe",
+    //             LastName: "Smith"
+    //         },
+    //             {
+    //                 FirstName: "Jane",
+    //                 LastName: "Smith"
+    //             }]
+    //     }
+    // });
+
+    // var dataSource = new kendo.data.DataSource({
+    //     transport: {
+    //         read:  {
+    //             url: "http://localhost:9000/api/values/GetProducts/",
+    //             dataType: "json" // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+    //         }
+    //     }
+    // });
+    // dataSource.fetch(function(){
+    //     var data = dataSource.data();
+    //     console.log(data.length);  // displays "77"
+    //     console.log(data[0].ProductName); // displays "Chai"
+    // });
+
+    var serviceUrl = 'http://localhost:9000/api/values/GetProducts/';
+
+    sendRequest = function() {
+        var method = $('#method').val();
+
+        $.ajax({
+            type: method,
+            url: serviceUrl
+        }).done(function (data) {
+            $('#value1').text(data);
+        }).error(function (jqXHR, textStatus, errorThrown) {
+            $('#value1').text(jqXHR.responseText || textStatus);
+        });
+    }
 });
